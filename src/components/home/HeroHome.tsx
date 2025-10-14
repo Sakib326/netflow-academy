ed006ceddc7f736dd5829b0a976998246e7790cc
sakib <smsakib2001@gmail.com>
2025-10-15 02:01:49 +0600

    Implement structural updates and optimizations across multiple modules

--- src/components/home/HeroHome.tsx
+++ src/components/home/HeroHome.tsx
@@ -31,7 +31,18 @@ const HeroHome = () => {
           <SwiperSlide className="owl-item">
             <div className="tw:w-full tw:aspect-[16/5] tw:relative">
               <Image
-                src="/assets/img/hero/hero-2.jpg"
+                src="/assets/img/hero/hero-2.jpeg"
+                alt="Hero slide 2"
+                fill
+                className="tw:object-contain tw:object-center"
+                sizes="100vw"
+              />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="owl-item">
+            <div className="tw:w-full tw:aspect-[16/5] tw:relative">
+              <Image
+                src="/assets/img/hero/hero-3.jpeg"
                 alt="Hero slide 2"
                 fill
                 className="tw:object-contain tw:object-center"
